077b9b926d1589e043cf56c67c63c9f69fbaea44
wachira <wewaruts@gmail.com>
2024-04-10 20:46:25 +0300

    uodated

--- index.js
+++ index.js
@@ -1,36 +1,65 @@
 const quizData = [
+    
     {
-        question: "Which language runs in a web browser",
-        a: "C",
-        b: "C++",
-        c: "JavaScript",
-        d: "Python",
-        correct:"c",
-    },
-    {
-        question: "What does HTML stand for?",
-        a: "HypeText Mark Language",
-        b: "HypeText Markup Language",
-        c: "HighText Markup Language",
-        d: "None of the above",
-        correct:"b",
-    },
-    {
-        question: "What does CSS stand for?",
-        a: "Cascading Style Sheets",
-        b: "Central Style Sheets",
-        c: "Cascading Simple Sheets",
-        d: "None of the above",
-        correct:"a",
-    },
-    {
-        question: "What are the three major languages for web design?",
-        a: "Java,HTML,CSS",
-        b: "JavaScript,C,HTML",
-        c: "JavaScript,HTML,CSS",
-        d: "None of the above",
-        correct:"c",
-    }
+        question: "1) The statement a===b refers to ?",
+        a: "Both a and b are equal in value, type and reference address",
+        b: "Both a and b are equal in value",
+        c: "Both a and b are equal in value and type",
+        d: "There is no such statement",
+       
+        correct: "c",
+      },
+     {
+        question: "2) Why event handlers is needed in JS?",
+        a:"Allows JavaScript code to alter the behaviour of windows",
+        b:  "Adds innerHTML page to the code",
+        c: "Change the server location",
+        d:  "Performs handling of exceptions and occurrences",
+        correct: "a",
+     },
+      {
+        question: "3) The pop() method of the array does which of the following task?",
+        a:"decrements the total length by 1" ,
+        b: "increments the total length by 1",
+        c:"prints the first element but no effect on the length",
+        d: " updates the element",
+        correct: "a",
+     },
+     {
+        question: "4) The primary purpose of the array map() function is that it?",
+        a: "maps the elements of another array into itself",
+        b: "passes each element of the array and returns the necessary mapped elements",
+        c: "passes each element of the array on which it is invoked to the function you specify, and returns an array containing the values returnedby that function ",
+        d: "pass the elements of the array into another array",
+        correct: "c",
+
+      },
+      {
+        question: "5) The 'var' and 'function' are?",
+        a: "Keywords",
+        b: "Declaration statements",
+        c: "Data types",
+        d: "Prototypes",
+        correct: "b",
+        
+       },
+       {
+        question: "6) JavaScript can be written_____?",
+        a:"directly into JS file and included into HTML",
+        b:"directly on the server page",
+        c:"directly into HTML pages",
+        d:"directly into the css file",
+        correct: "a",
+       },
+       {
+        question: "7) What is the fundamental rule of lexical scoping?",
+        a:"Functions are declared in the scope",
+        b:"Functions are executed using scope chain",
+        c:"Functions are declared outside the scope",
+        d:"Variables are declared within the function",
+        correct: "b",
+       },
+    
 ];
 const quiz = document.getElementById("quiz");
 const answersA1 = document.querySelectorAll(".answer");
